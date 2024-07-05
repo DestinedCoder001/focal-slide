762c85948aae9e556857ea4a7f276ed9b0301653
Olowokere-Destiny <destinyolowokere@gmail.com>
2024-07-05 23:14:49 +0100

    slide issue fix

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
-var swiper = new Swiper('.swiper-container', {
-    loop: true,
-    autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        renderBullet: function (_, className) {
-            return '<span class="' + className + '"></span>';
-        },
-    },
-    on: {
-        slideChangeTransitionStart: function () {
-            document.querySelectorAll('.swiper-pagination-bullet-active').forEach(function (bullet) {
-                bullet.style.setProperty('animation', 'none');
-                setTimeout(function () {
-                    bullet.style.setProperty('animation', '');
-                }, 10);
-            });
-        },
-    },
+const swiper = new Swiper(".swiper-container", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 40,
+    modifier: 1,
+    slideShadows: false,
+  },
+  loop: true,
+  speed: 300,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
 });
